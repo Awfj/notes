af65c55035dc4e9ce1c7e764558f20623dd7cfe4
Alexey Kokhanovsky <awfj837@gmail.com>
2019-09-19 19:00:52 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    notes: ["a", "b"],
+    value: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const value = e.target["new note"].value;
+    if (!value) return;
+
+    const notes = this.state.notes.concat(value);
+    this.setState({ notes });
+  };
+
+  removeNote = id => {
+    const notes = [...this.state.notes];
+    notes.splice(id, 1);
+    this.setState({ notes });
+  };
+
+  render() {
+    // console.log(this.state.notes);
+
+    const notes =
+      this.state.notes.length > 0 ? (
+        <ul>
+          {this.state.notes.map((note, index) => (
+            <li key={index}>
+              {note}
+              <button type="button" onClick={this.removeNote.bind(this, index)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null;
+
+    return (
+      <div className="App">
+        <header className="">
+          <h1>Notes</h1>
+        </header>
+        <main>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Take a note..." name="new note" />
+            <button>Submit</button>
+          </form>
+          {notes}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
