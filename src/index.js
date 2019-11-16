47e3649d03389e3ce5505ed8b6cd2d921f31d74a
Alexey Kokhanovsky <awfj837@gmail.com>
2019-11-16 16:19:12 +0300

    uninstall normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import "normalize.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import App from "./components/App/App";
